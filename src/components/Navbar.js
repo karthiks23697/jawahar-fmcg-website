13fef4585614c0fc51f0c780cee53dcc74292c04
karthiks23697 <karthiks23697@gmail.com>
2025-03-26 22:57:54 +0530

    navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,9 +10,9 @@ function Navbar() {
             FMCG Store
           </Typography>
           <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/products">Products</Button>
-          <Button color="inherit" component={Link} to="/about">About</Button>
-          <Button color="inherit" component={Link} to="/contact">Contact</Button>
+          <Button color="inherit" component={Link} to="/">Products</Button>
+          <Button color="inherit" component={Link} to="/">About</Button>
+          <Button color="inherit" component={Link} to="/">Contact</Button>
         </Toolbar>
       </Container>
     </AppBar>
